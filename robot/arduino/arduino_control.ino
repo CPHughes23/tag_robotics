58c31ee14fbe5ed865a4f307d11ae7acd817b0ef
CPHughes23 <139437105+CPHughes23@users.noreply.github.com>
2026-04-09 14:30:29 -0700

    feat: update control code to working at library

--- robot/arduino/arduino_control.ino
+++ robot/arduino/arduino_control.ino
@@ -1,19 +1,33 @@
 void setup() {
-    pinMode(10, OUTPUT);
-    pinMode(LED_BUILTIN, OUTPUT)
-    Serial.begin(9600)
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-    if (Serial.available()) {
-        char cmd = Serial.read();
-        if (cmd == '1') {
-            digitalWrite(10, HIGH);
-            digitalWrite(LED_BUILTIN, HIGH);
-        }
-        if (cmd == '0') {
-            digitalWrite(10, LOW);
-            digitalWrite(LED_BUILTIN, LOW);
-        }
+  if (Serial.available()) {
+    char cmd = Serial.read();
+
+    if (cmd == '4') {
+      // stop all
+      digitalWrite(4, LOW);
+      digitalWrite(5, LOW);
+      digitalWrite(6, LOW);
+      digitalWrite(7, LOW);
+      digitalWrite(LED_BUILTIN, LOW);
     }
+
+    // forward/back pins
+    if (cmd == '0') { digitalWrite(4, HIGH); digitalWrite(7, LOW); }  // forward on
+    if (cmd == '1') { digitalWrite(7, HIGH); digitalWrite(4, LOW); }  // reverse on
+    if (cmd == 'f') { digitalWrite(4, LOW);  digitalWrite(7, LOW); }  // forward/back off
+
+    // left/right pins
+    if (cmd == '2') { digitalWrite(6, HIGH); digitalWrite(5, LOW); }  // left on
+    if (cmd == '3') { digitalWrite(5, HIGH); digitalWrite(6, LOW); }  // right on
+    if (cmd == 'r') { digitalWrite(6, LOW);  digitalWrite(5, LOW); }  // left/right off
+  }
 }
--- robot/arduino/serial_sender.py
+++ robot/arduino/serial_sender.py
@@ -1,28 +1,62 @@
 import serial
 import time
+from pynput import keyboard
 
-# This connects to device 110 over serial on 9600 (find this info in arduino IDE)
-ser = serial.Serial('/dev/cu.usbserial-110', 9600)
-time.sleep(2) # wait for arduino to reset
+ser = serial.Serial('/dev/cu.usbserial-2110', 9600)
+time.sleep(2)
 
-def press():
-    ser.write(b'1')
+STOP = b'4'
 
-def release():
-    ser.write(b'0')
+KEY_COMMANDS = {
+    'w': b'0',  # forward
+    'a': b'2',  # left
+    's': b'1',  # reverse
+    'd': b'3',  # right
+}
 
-def main():
-    while True:
-        cmd = input("Enter command (1 for on, 0 for off, q to quit)")
+pressed_keys = set()
 
-        if cmd == '1':
-            press()
-        elif cmd == '0':
-            release()
-        elif cmd == 'q':
-            break
-        else:
-            print("Unknown command")
+def send_command():
+    fwd = 'w' in pressed_keys
+    rev = 's' in pressed_keys
+    lft = 'a' in pressed_keys
+    rgt = 'd' in pressed_keys
 
-if __name__ == '__main__':
-    main()
+    # forward/back axis
+    if fwd:        ser.write(b'0')
+    elif rev:      ser.write(b'1')
+    else:          ser.write(b'f')  # stop forward/back axis only
+
+    # left/right axis
+    if lft:        ser.write(b'2')
+    elif rgt:      ser.write(b'3')
+    else:          ser.write(b'r')  # stop left/right axis only
+
+def on_press(key):
+    try:
+        k = key.char
+        if k in KEY_COMMANDS:
+            if k not in pressed_keys:
+                pressed_keys.add(k)
+                send_command()
+        elif k == 'q':
+            ser.write(STOP)
+            ser.close()
+            return False  # stop listener
+    except AttributeError:
+        pass  # ignore special keys
+
+def on_release(key):
+    try:
+        k = key.char
+        if k in KEY_COMMANDS and k in pressed_keys:
+            pressed_keys.discard(k)
+            send_command()
+    except AttributeError:
+        pass
+
+print("Controls: W/A/S/D to drive, Q to quit")
+print("Hold a key to move, release to stop.\n")
+
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
